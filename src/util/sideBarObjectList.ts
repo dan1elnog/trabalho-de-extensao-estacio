d92323dfc7802e8e9a8047db9b0b833ebea326ce
Daniel Nogueira <daniel.00msn@gmail.com>
2024-11-10 22:24:47 -0300

    realizando pequenos ajustes

--- src/util/sideBarObjectList.ts
+++ src/util/sideBarObjectList.ts
@@ -1,4 +1,4 @@
-import { FaAws, FaCss3, FaDatabase, FaHouse, FaHtml5, FaJava, FaJs, FaLinux, FaPhp, FaPython } from "react-icons/fa6";
+import { FaAws, FaCss3, FaDatabase, FaHouse, FaHtml5, FaJava, FaJs, FaPhp, FaPython } from "react-icons/fa6";
 import { SideBarObjectType } from "../types/SideBarObjectType";
 import { PagesEnum } from "../enums/PagesEnum";
 
@@ -11,8 +11,7 @@ const sideBarObjectList: SideBarObjectType[] = [
   { id: 6, title: "Python", icon: FaPython, page: PagesEnum.PYTHON_PAGE},
   { id: 7, title: "JavaScript", icon: FaJs, page: PagesEnum.JAVASCRIPT_PAGE},
   { id: 8, title: "Banco de dados", icon: FaDatabase, page: PagesEnum.DATABASE },
-  { id: 9, title: "AWS", icon: FaAws, page: PagesEnum.AWS_PAGE},
-  // { id: 10, title: "Linux", icon: FaLinux },
+  { id: 9, title: "AWS", icon: FaAws, page: PagesEnum.AWS_PAGE}
 ]
 
 export default sideBarObjectList
